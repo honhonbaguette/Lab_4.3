470a933a4a4099f3f1a5f533e2830b0970b9050a
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2018-03-29 09:22:22 -0400

    (;_;)

--- Lab-4.3/src/FastClicker/GameCode.java
+++ Lab-4.3/src/FastClicker/GameCode.java
@@ -23,27 +23,29 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
-public class GameCode extends Application {
-    public static void main(String[] args) {
+public class GameCode extends Application 
+{
+	private boolean scoreing;
+	private int scores;
+	private long timer;
+	
+    public static void main(String[] args) 
+    {
         launch(args);
     }
     
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage)
+    {
+    	primaryStage.setTitle("FASTEST CLICKER");
+    	Button butt = new Button;
+    	butt.setText("HOW FAST CAN YA CLICK?");
+    	butt.setOnAction
+    }
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 }
