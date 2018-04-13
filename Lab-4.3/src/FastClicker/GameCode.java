b91b1cc4ed669f7ab14609c4e3298fd08e7a06a0
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2018-04-13 09:19:31 -0400

    not like this ;_;

--- Lab-4.3/src/FastClicker/GameCode.java
+++ Lab-4.3/src/FastClicker/GameCode.java
@@ -1,33 +1,21 @@
 package FastClicker;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
- 
 public class GameCode extends Application 
 {
-	private boolean scoreing;
+	private boolean scoring;
 	private int scores;
 	private long timer;
+	Labeled text;
 	
     public static void main(String[] args) 
     {
@@ -38,14 +26,51 @@ public class GameCode extends Application
     public void start(Stage primaryStage)
     {
     	primaryStage.setTitle("FASTEST CLICKER");
-    	Button butt = new Button;
-    	butt.setText("HOW FAST CAN YA CLICK?");
-    	butt.setOnAction
+    	Button butt = new Button();
+    	butt.setText("HOW FAST CAN YE CLICK?");
+    	butt.setText("Click");
+    	butt.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            @Override
+            public void handle(ActionEvent event) 
+            {
+               if(!scoring)
+               {
+            	   scores++;
+               }
+            }
+        });
+        
+        timer = System.nanoTime() + 10000000000L;
+        new AnimationTimer()
+        {
+			@Override
+			public void handle(long now) 
+			{
+				if(now > timer)
+				{
+					timer = now + 10000000000L;
+					scoring = !scoring;
+				}
+				if(!scoring)
+				{
+					butt.setText("CLICK ME REAL FAST NOW!");
+				}
+				else
+				{
+					butt.setText("Score: " + Integer.toString(scores));
+					
+				}
+				
+			}
+        	
+        }.start();
     }
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 500, 500));
+        root.getChildren().add(butt);
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
+        
     }
 }
